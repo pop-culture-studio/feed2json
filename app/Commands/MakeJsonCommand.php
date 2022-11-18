ec9ca9a84df6b65c9e83aed0e34fd5bea3bff988
PCS <pcs.engineer.team@gmail.com>
2022-11-18 12:11:20 +0900

    feed2json

---
+++ app/Commands/MakeJsonCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Storage;
+use LaravelZero\Framework\Commands\Command;
+
+class MakeJsonCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'feed:json';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->note();
+
+        return 0;
+    }
+
+    protected function note()
+    {
+        $items = collect();
+
+        $xml = simplexml_load_file('https://note.com/pcs_miraizu/rss');
+
+        if (! $xml) {
+            return;
+        }
+
+        foreach ($xml->channel->item as $item) {
+            $items->push([
+                'title' => (string) $item->title,
+                'link' => (string) $item->link,
+                'date' => Carbon::parse($item->pubDate)->toDateTimeString(),
+            ]);
+        }
+
+        Storage::put('note_pcs_miraizu.json', $items->toJson());
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
---
+++ app/Discord/Directs/DmTestCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Discord\Directs;
+
+use Discord\Parts\Channel\Message;
+
+class DmTestCommand
+{
+    /**
+     * @var string
+     */
+    public string $command = 'test';
+
+    /**
+     * @param  Message  $message
+     * @return void
+     */
+    public function __invoke(Message $message)
+    {
+        $message->reply('dm test! '.$message->author->username)
+                ->done(function (Message $message) {
+                });
+    }
+}
